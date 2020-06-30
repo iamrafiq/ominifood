22aec3612ead38699971fa3b852eabde7fe64662
linuxrafiq <rafiq.i@live.co.uk>
2020-07-01 00:59:33 +0600

    animation with waypoint and https://animate.style/

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,20 +1,112 @@
 $(document).ready(function () {
-    $('.js--section-features').waypoint(function(direction){
-        if(direction=='down'){
-            $('nav').addClass('sticky');
-        }else{
-            $('nav').removeClass('sticky');
-        }
-    },
-    {
-      offset: "60px",
-    })
-  /*var waypoints = $("#handler-first").waypoint(
+  /**
+   * For sticky navigation
+   */
+  $(".js--section-features").waypoint(
     function (direction) {
-      notify(this.element.id + " hit 25% from top of window");
+      if (direction == "down") {
+        $("nav").addClass("sticky");
+      } else {
+        $("nav").removeClass("sticky");
+      }
     },
     {
-      offset: "25%",
+      //offset: "25%" can be set value as percentage
+      offset: "60px",
     }
-  );*/
+  );
+
+  $(".js--scroll-to-plans").click(function () {
+    $("body,html").animate(
+      {
+        scrollTop: $(".js--section-plans").offset().top,
+      },
+      1000
+    );
+    return false;
+  });
+
+  $(".js--scroll-to-start").click(function () {
+    $("body,html").animate(
+      {
+        scrollTop: $(".js--section-features").offset().top,
+      },
+      1000
+    );
+    return false;
+  });
+
+  $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+      // On-page links
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        // Does a scroll target exist?
+        if (target.length) {
+          // Only prevent default if animation is actually gonna happen
+          event.preventDefault();
+          $("html, body").animate(
+            {
+              scrollTop: target.offset().top,
+            },
+            1000,
+            function () {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) {
+                // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              }
+            }
+          );
+        }
+      }
+    });
+  /*Animation on scroll https://animate.style/*/
+  $(".js--wp-1").waypoint(
+    function (direction) {
+      $(".js--wp-1").addClass("fadeIn");
+    },
+    {
+      offset: "50%",
+    }
+  );
+  $(".js--wp-2").waypoint(
+    function (direction) {
+      $(".js--wp-2").addClass("fadeInUp");
+    },
+    {
+      offset: "50%",
+    }
+  );$(".js--wp-3").waypoint(
+    function (direction) {
+      $(".js--wp-3").addClass("fadeIn");
+    },
+    {
+      offset: "50%",
+    }
+  );$(".js--wp-4").waypoint(
+    function (direction) {
+      $(".js--wp-4").addClass("pulse");
+    },
+    {
+      offset: "50%",
+    }
+  );
 });
